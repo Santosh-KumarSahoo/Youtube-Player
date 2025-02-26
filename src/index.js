ec495501cf383ac5279dcdc38080fe378a125eed
Santosh Kumar Sahoo <santohkumarsahoo8335@gmail.com>
2025-02-26 11:44:35 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -11,6 +11,17 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running at post : ${process.env.PORT}`);
+  })
+})
+.catch((err) => {
+  console.log("MONGODB connection FAILED !!! ", err);
+})
+
+
+
 
 // import express from "express";
 // const app = express()
